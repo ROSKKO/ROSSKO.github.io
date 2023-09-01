cf080981619b9a24dd1568224e2ffda02ddf82b0
ROSKKO <roskkopro@gmail.com>
2023-09-01 01:49:34 -0300

    Update data.php

--- data.php
+++ data.php
@@ -2,9 +2,8 @@
 <html>
 <body>
 
-Welcome <?php
-echo $_POST["username"];?><br>
-Password <?php echo $_POST["password"] ?>    
+Welcome <?php echo $_POST["username"];?><br>
+Password <?php echo $_POST["password"]; ?>    
 
 
 
